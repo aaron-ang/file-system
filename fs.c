058e65266bd1f0beb5b8cd526c58dc5da33fc71e
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2024-03-28 15:08:49 -0400

    complete fs_create

--- fs.c
+++ fs.c
@@ -47,20 +47,20 @@ struct file_descriptor {
 
 union fs_block {
   struct super_block super;
-  struct dir_entry dir[DIR_ENTRIES_PER_BLOCK];
+  struct dir_entry dir_table[DIR_ENTRIES_PER_BLOCK];
   uint8_t inode_bitmap[MAX_FILES / CHAR_BIT];
   uint8_t used_block_bitmap[DISK_BLOCKS / CHAR_BIT];
-  struct inode inodes[INODES_PER_BLOCK];
+  struct inode inode_table[INODES_PER_BLOCK];
   uint16_t block_offsets[DIRECT_OFFSETS_PER_BLOCK];
   char data[BLOCK_SIZE];
 };
 
 // on-disk and in-memory
 struct super_block sb;
-struct dir_entry dir[MAX_FILES];
+struct dir_entry dir_table[MAX_FILES];
 uint8_t inode_bitmap[MAX_FILES / CHAR_BIT];
 uint8_t used_block_bitmap[DISK_BLOCKS / CHAR_BIT];
-struct inode inodes[MAX_FILES];
+struct inode inode_table[MAX_FILES];
 
 // in-memory
 bool is_mounted = false;
@@ -68,14 +68,21 @@ struct file_descriptor fds[MAX_FD];
 
 // Helper functions
 
-// Returns the index of the file in the directory table.
-int get_file_index(const char *name) {
+struct dir_entry *get_dentry(const char *name) {
   for (int i = 0; i < MAX_FILES; i++) {
-    if (dir[i].is_used && strcmp(dir[i].name, name) == 0) {
-      return i;
+    if (dir_table[i].is_used && strcmp(dir_table[i].name, name) == 0)
+      return &dir_table[i];
+  }
+  return NULL;
+}
+
+struct dir_entry *get_unused_dentry() {
+  for (int i = 0; i < MAX_FILES; i++) {
+    if (dir_table[i].is_used == false) {
+      return &dir_table[i];
     }
   }
-  return -1;
+  return NULL;
 }
 
 bool bitmap_test(uint8_t *bitmap, int idx) {
@@ -88,13 +95,21 @@ void bitmap_set(uint8_t *bitmap, int idx, bool val) {
   bitmap[idx / CHAR_BIT] ^= 1 << (idx % CHAR_BIT);
 }
 
+int get_unused_inum() {
+  for (int i = 0; i < MAX_FILES; i++) {
+    if (bitmap_test(inode_bitmap, i) == 0)
+      return i;
+  }
+  return -1;
+}
+
 // Returns the block number of the data block at the given file offset.
 // Returns -1 if the block is not allocated
 int get_data_block_offset(uint16_t inum, int file_offset) {
   assert(inum >= 0 && inum < MAX_FILES);
   assert(file_offset >= 0 && file_offset < MAX_FILE_SIZE);
 
-  struct inode inode = inodes[inum];
+  struct inode inode = inode_table[inum];
   uint16_t block_num;
   int block_offset = file_offset / BLOCK_SIZE;
 
@@ -104,37 +119,37 @@ int get_data_block_offset(uint16_t inum, int file_offset) {
     return block_num == 0 ? -1 : block_num;
   }
 
-  union fs_block block;
+  union fs_block block_buffer;
   block_offset -= DIRECT_OFFSETS_PER_INODE;
 
   // single indirect
   if (block_offset < DIRECT_OFFSETS_PER_BLOCK) {
     assert(inode.single_indirect_offset != 0);
-    if (block_read(inode.single_indirect_offset, &block)) {
+    if (block_read(inode.single_indirect_offset, &block_buffer)) {
       fprintf(stderr,
               "get_data_block_offset: failed to read single indirect block\n");
       return -1;
     }
-    block_num = block.block_offsets[block_offset];
+    block_num = block_buffer.block_offsets[block_offset];
     return block_num == 0 ? -1 : block_num;
   }
 
   // double indirect
   block_offset -= DIRECT_OFFSETS_PER_BLOCK;
   assert(inode.double_indirect_offset != 0);
-  if (block_read(inode.double_indirect_offset, &block)) {
+  if (block_read(inode.double_indirect_offset, &block_buffer)) {
     fprintf(stderr,
             "get_data_block_offset: failed to read double indirect block\n");
     return -1;
   }
   block_offset /= DIRECT_OFFSETS_PER_BLOCK;
-  if (block_read(block.block_offsets[block_offset], &block)) {
+  if (block_read(block_buffer.block_offsets[block_offset], &block_buffer)) {
     fprintf(stderr,
             "get_data_block_offset: failed to read double indirect block\n");
     return -1;
   }
   block_offset %= DIRECT_OFFSETS_PER_BLOCK;
-  block_num = block.block_offsets[block_offset];
+  block_num = block_buffer.block_offsets[block_offset];
   return block_num == 0 ? -1 : block_num;
 }
 
@@ -157,9 +172,9 @@ int make_fs(const char *disk_name) {
   sb.data_offset = 5;
 
   // write super block
-  union fs_block block;
-  block.super = sb;
-  if (block_write(0, &block)) {
+  union fs_block block_buffer;
+  block_buffer.super = sb;
+  if (block_write(0, &block_buffer)) {
     fprintf(stderr, "make_fs: failed to write super block\n");
     return -1;
   }
@@ -168,9 +183,10 @@ int make_fs(const char *disk_name) {
   for (int i = 0; i < METADATA_BLOCKS; i++) {
     bitmap_set(used_block_bitmap, i, 1);
   }
-  memset(&block, 0, BLOCK_SIZE);
-  memcpy(block.used_block_bitmap, used_block_bitmap, sizeof(used_block_bitmap));
-  if (block_write(sb.used_block_bitmap_offset, &block)) {
+  memset(&block_buffer, 0, BLOCK_SIZE);
+  memcpy(block_buffer.used_block_bitmap, used_block_bitmap,
+         sizeof(used_block_bitmap));
+  if (block_write(sb.used_block_bitmap_offset, &block_buffer)) {
     fprintf(stderr, "make_fs: failed to write used block bitmap\n");
     return -1;
   }
@@ -189,14 +205,14 @@ int mount_fs(const char *disk_name) {
     return -1;
   }
 
-  union fs_block block;
+  union fs_block block_buffer;
 
   // read super block
-  if (block_read(0, &block)) {
+  if (block_read(0, &block_buffer)) {
     fprintf(stderr, "mount_fs: failed to read super block\n");
     return -1;
   }
-  struct super_block sb_slice = block.super;
+  struct super_block sb_slice = block_buffer.super;
   if (sb_slice.dir_table_offset == 0) {
     fprintf(stderr, "mount_fs: file system not initialized\n");
     return -1;
@@ -204,32 +220,33 @@ int mount_fs(const char *disk_name) {
   sb = sb_slice;
 
   // read directory table
-  if (block_read(sb_slice.dir_table_offset, &block)) {
+  if (block_read(sb_slice.dir_table_offset, &block_buffer)) {
     fprintf(stderr, "mount_fs: failed to read directory table\n");
     return -1;
   }
-  memcpy(dir, block.dir, sizeof(dir));
+  memcpy(dir_table, block_buffer.dir_table, sizeof(dir_table));
 
   // read inode bitmap
-  if (block_read(sb_slice.inode_metadata_offset, &block)) {
+  if (block_read(sb_slice.inode_metadata_offset, &block_buffer)) {
     fprintf(stderr, "mount_fs: failed to read inode bitmap\n");
     return -1;
   }
-  memcpy(inode_bitmap, block.inode_bitmap, sizeof(inode_bitmap));
+  memcpy(inode_bitmap, block_buffer.inode_bitmap, sizeof(inode_bitmap));
 
   // read used block bitmap
-  if (block_read(sb_slice.used_block_bitmap_offset, &block)) {
+  if (block_read(sb_slice.used_block_bitmap_offset, &block_buffer)) {
     fprintf(stderr, "mount_fs: failed to read used block bitmap\n");
     return -1;
   }
-  memcpy(used_block_bitmap, block.used_block_bitmap, sizeof(used_block_bitmap));
+  memcpy(used_block_bitmap, block_buffer.used_block_bitmap,
+         sizeof(used_block_bitmap));
 
   // read inode table
-  if (block_read(sb_slice.inode_offset, &block)) {
+  if (block_read(sb_slice.inode_offset, &block_buffer)) {
     fprintf(stderr, "mount_fs: failed to read inodes\n");
     return -1;
   }
-  memcpy(inodes, block.inodes, sizeof(inodes));
+  memcpy(inode_table, block_buffer.inode_table, sizeof(inode_table));
 
   is_mounted = true;
   return 0;
@@ -241,43 +258,44 @@ int umount_fs(const char *disk_name) {
     return -1;
   }
 
-  union fs_block block;
+  union fs_block block_buffer;
 
   // write super block
-  block.super = sb;
-  if (block_write(0, &block)) {
+  block_buffer.super = sb;
+  if (block_write(0, &block_buffer)) {
     fprintf(stderr, "umount_fs: failed to write super block\n");
     return -1;
   }
 
   // write directory table
-  memset(&block, 0, BLOCK_SIZE);
-  memcpy(block.dir, dir, sizeof(dir));
-  if (block_write(sb.dir_table_offset, &block)) {
+  memset(&block_buffer, 0, BLOCK_SIZE);
+  memcpy(block_buffer.dir_table, dir_table, sizeof(dir_table));
+  if (block_write(sb.dir_table_offset, &block_buffer)) {
     fprintf(stderr, "umount_fs: failed to write directory table\n");
     return -1;
   }
 
   // write inode bitmap
-  memset(&block, 0, BLOCK_SIZE);
-  memcpy(block.inode_bitmap, inode_bitmap, sizeof(inode_bitmap));
-  if (block_write(sb.inode_metadata_offset, &block)) {
+  memset(&block_buffer, 0, BLOCK_SIZE);
+  memcpy(block_buffer.inode_bitmap, inode_bitmap, sizeof(inode_bitmap));
+  if (block_write(sb.inode_metadata_offset, &block_buffer)) {
     fprintf(stderr, "umount_fs: failed to write inode bitmap\n");
     return -1;
   }
 
   // write used block bitmap
-  memset(&block, 0, BLOCK_SIZE);
-  memcpy(block.used_block_bitmap, used_block_bitmap, sizeof(used_block_bitmap));
-  if (block_write(sb.used_block_bitmap_offset, &block)) {
+  memset(&block_buffer, 0, BLOCK_SIZE);
+  memcpy(block_buffer.used_block_bitmap, used_block_bitmap,
+         sizeof(used_block_bitmap));
+  if (block_write(sb.used_block_bitmap_offset, &block_buffer)) {
     fprintf(stderr, "umount_fs: failed to write used block bitmap\n");
     return -1;
   }
 
   // write inode table
-  memset(&block, 0, BLOCK_SIZE);
-  memcpy(block.inodes, inodes, sizeof(inodes));
-  if (block_write(sb.inode_offset, &block)) {
+  memset(&block_buffer, 0, BLOCK_SIZE);
+  memcpy(block_buffer.inode_table, inode_table, sizeof(inode_table));
+  if (block_write(sb.inode_offset, &block_buffer)) {
     fprintf(stderr, "umount_fs: failed to write inodes\n");
     return -1;
   }
@@ -297,8 +315,8 @@ int fs_open(const char *name) {
     fprintf(stderr, "fs_open: file system not mounted\n");
     return -1;
   }
-  int file_index = get_file_index(name);
-  if (file_index < 0) {
+  struct dir_entry *dentry = get_dentry(name);
+  if (dentry == NULL) {
     fprintf(stderr, "fs_open: file not found\n");
     return -1;
   }
@@ -306,7 +324,7 @@ int fs_open(const char *name) {
     struct file_descriptor fd = fds[fildes];
     if (fd.is_used == false) {
       fd.is_used = true;
-      fd.inode_number = dir[file_index].inode_number;
+      fd.inode_number = dentry->inode_number;
       fd.offset = 0;
       return fildes;
     }
@@ -340,7 +358,7 @@ int fs_create(const char *name) {
     fprintf(stderr, "fs_create: invalid file name\n");
     return -1;
   }
-  if (get_file_index(name) >= 0) {
+  if (get_dentry(name)) {
     fprintf(stderr, "fs_create: file already exists\n");
     return -1;
   }
@@ -349,9 +367,14 @@ int fs_create(const char *name) {
     return -1;
   }
 
-  // TODO: allocate inode and data blocks
-  // int block_num = sb.inode_metadata_offset + (inumber / INODES_PER_BLOCK);
-  // int block_offset = inumber % INODES_PER_BLOCK;
+  int inum = get_unused_inum();
+  assert(inum != -1);
+  bitmap_set(inode_bitmap, inum, 1);
+
+  struct dir_entry *dentry = get_unused_dentry();
+  dentry->is_used = true;
+  dentry->inode_number = inum;
+  dentry->name = name;
   return 0;
 }
 
@@ -360,14 +383,14 @@ int fs_delete(const char *name) {
     fprintf(stderr, "fs_delete: file system not mounted\n");
     return -1;
   }
-  int i = get_file_index(name);
-  if (i < 0) {
+  struct dir_entry *dentry = get_dentry(name);
+  if (dentry == NULL) {
     fprintf(stderr, "fs_delete: file not found\n");
     return -1;
   }
   for (int fildes = 0; fildes < MAX_FD; fildes++) {
     struct file_descriptor fd = fds[fildes];
-    if (fd.is_used && dir[i].inode_number == fd.inode_number) {
+    if (fd.is_used && dentry->inode_number == fd.inode_number) {
       fprintf(stderr, "fs_delete: file is open\n");
       return -1;
     }
@@ -392,8 +415,8 @@ int fs_read(int fildes, void *buf, size_t nbyte) {
     return -1;
   }
 
-  union fs_block block;
-  if (block_read(block_offset, &block)) {
+  union fs_block block_buffer;
+  if (block_read(block_offset, &block_buffer)) {
     fprintf(stderr, "fs_read: failed to read data block\n");
     return -1;
   }
@@ -406,7 +429,7 @@ int fs_read(int fildes, void *buf, size_t nbyte) {
       if (block_offset < 0) {
         break;
       }
-      if (block_read(block_offset, &block)) {
+      if (block_read(block_offset, &block_buffer)) {
         fprintf(stderr, "fs_read: failed to read data block\n");
         return -1;
       }
@@ -414,7 +437,8 @@ int fs_read(int fildes, void *buf, size_t nbyte) {
     }
     size_t bytes_to_read = nbyte - bytes_read;
     bytes_to_read = min(bytes_to_read, BLOCK_SIZE - offset_in_block);
-    memcpy(buf + bytes_read, block.data + offset_in_block, bytes_to_read);
+    memcpy(buf + bytes_read, block_buffer.data + offset_in_block,
+           bytes_to_read);
     bytes_read += bytes_to_read;
     offset_in_block += bytes_to_read;
   }
@@ -437,14 +461,14 @@ int fs_get_filesize(int fildes) {
     fprintf(stderr, "fs_get_filesize: invalid file descriptor\n");
     return -1;
   }
-  return inodes[fd.inode_number].file_size;
+  return inode_table[fd.inode_number].file_size;
 }
 
 int fs_listfiles(char ***files) {
   char *file = **files;
   for (int i = 0; i < MAX_FILES; i++) {
-    if (dir[i].is_used) {
-      const char *file_name = dir[i].name;
+    if (dir_table[i].is_used) {
+      const char *file_name = dir_table[i].name;
       if (file_name == NULL) {
         fprintf(stderr, "fs_listfiles: invalid file name\n");
         return -1;
@@ -467,7 +491,7 @@ int fs_lseek(int fildes, off_t offset) {
     fprintf(stderr, "fs_lseek: invalid file descriptor\n");
     return -1;
   }
-  if (offset > inodes[fd.inode_number].file_size) {
+  if (offset > inode_table[fd.inode_number].file_size) {
     fprintf(stderr, "fs_lseek: offset exceeds file size\n");
     return -1;
   }
@@ -485,7 +509,7 @@ int fs_truncate(int fildes, off_t length) {
     fprintf(stderr, "fs_truncate: invalid file descriptor\n");
     return -1;
   }
-  int file_size = inodes[fd.inode_number].file_size;
+  int file_size = inode_table[fd.inode_number].file_size;
   if (length < 0 || length > file_size) {
     fprintf(stderr, "fs_truncate: invalid length\n");
     return -1;
